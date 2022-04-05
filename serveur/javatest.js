923d60043b1caf73c251d32d7e1b337d78d83430
Turbotard <turbotard9@gmail.com>
2022-04-05 18:48:08 +0200

    CA MARCHE

--- serveur/javatest.js
+++ serveur/javatest.js
@@ -1,23 +1,20 @@
+import postgres from 'postgres'
 
 
-const mysql = require('mysql');
+const sql = postgres('postgres://postgres:azertyuiop@localhost:5432/test', {
+  port                 : 5432,          // Postgres server port[s]
 
-const con = mysql.createConnection({
-   host: "localhost",
-   user: "nom_utilisateur",
-   password: "mot_de_passe_utilisateur",
-   database : "mabdd"
- });
-
-  con.connect(function(err) {
-   if (err) throw err;
-   console.log("Connecté à la base de données MySQL!");
-   con.query("SELECT eleves.id as 'eleve_id', eleves.nom as 'eleve_nom', eleves.cours_id, cours.nom as 'cours_nom', cours.date as 'cours_date' FROM eleves JOIN cours on eleves.cours_id = cours.id", function (err, result) {
-       if (err) throw err;
-       console.log(result);
-     });
- });
+})
 
 
 
+async function recuptout() {
+  const users = await sql`
+    select * from table_test
+  `
+  // users = Result [{ name: "Murray", age: 68 }]
+  return users
+}
 
+const resultat = await recuptout()
+console.log(resultat)
